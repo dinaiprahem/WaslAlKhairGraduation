bb54e9fb9c6314234d21b095fe912433a431c320
Fatmaa17 <fatma00mahmoud0@gmail.com>
2025-03-05 23:06:45 +0200

    Created SMS service with its endpoint

---
+++ DTOs/SMS/DedicationSMSDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WaslAlkhair.Api.DTOs.SMS
+{
+    public class DedicationSMSDto
+    {
+        public string MobileNumber { get; set; }
+        public string Body { get; set; }
+        //public string SenderName { get; set; }
+       // public decimal DonationAmount { get; set; }
+    }
+}
